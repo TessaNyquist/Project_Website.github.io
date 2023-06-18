f0563b7c03ace3724c19403a098c2e6ce5a14d38
TessaNyquist <tessanyquist@outlook.com>
2023-06-18 11:54:13 -0400

    Working with JSX

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,20 +1,46 @@
 //import logo from './logo.svg';
 //<img src={logo} className="App-logo" alt="logo" />
-import React, { useState } from 'react';
-import ButtonBox from "./Components/Buttonbox";
-import Button from "./Components/Button";
+//import React, { useState } from 'react';
+//import ButtonBox from "./Components/Buttonbox";
+//import Button from "./Components/Button";
+//import Navbar from "./Components/ContainerInsideExample";
 import './App.css';
 
-const btnValues = ["Educational Background","Work Experience"];
-
-function App() {
+/*function Button ({value}) {
   return (
-    <div className="header">
-      <ButtonBox> {btnValues.flat().map((btn, i) => { return ();}
-      </ButtonBox>
-      
-    </div>
+  <button> 
+    {value} 
+  </button>);
+}
+*/
+
+/*function ButtonBox ({ buttons}) {
+  //buttons.forEach( <Button value={text} /> );  
+  return (
+    <title> Portfolio </title>
   );
 }
+*/
 
-export default App;
+function AboutMe (){
+  let bio = "A native Northern Virginian, I completed my B.A. in political science and B.S. in computer science from James Madison University (JMU) in 2022, and received the NSA Information Systems Security Professionals Certification.  While attending JMU, I assisted fellow students navigate the university’s disciplinary process while I served on the executive board of Student  Defenders. Currently, I am serving as a legal assistant at an employment law firm in Washington, D.C., where I assist prospective clients in navigating the firm’s intake process, while leveraging my background in computer science to automate processes. I am passionate about serving back in the community I grew up in and is interested in pursuing opportunities in the tech industry in the Washington, D.C. metro.";
+  //let imgsrc = "./Images/Tessa-Banner.png"
+  return (
+    <div>
+      <h2> About Me</h2>
+
+      <p>{bio}</p>
+    </div>
+  );
+  //  <h2 style={{ fontFamily: '"Times New Roman", Times' }}> 
+  //  <img class="w3-image" src="Images/Tessa-Banner.png"width="1500" height="800">
+  //  <p>A native Northern Virginian, I completed my B.A. in political science and B.S. in computer science from James Madison University (JMU) in 2022, and received the NSA Information Systems Security Professionals Certification. While attending JMU, I assisted fellow students navigate the university’s disciplinary process while I served on the executive board of Student Defenders. Currently, I am serving as a legal assistant at an employment law firm in Washington, D.C., where I assist prospective clients in navigating the firm’s intake process, while leveraging my background in computer science to automate processes. I am passionate about serving back in the community I grew up in and is interested in pursuing opportunities in the tech industry in the Washington, D.C. metro.  </p>
+  //);
+}
+
+//const NAMES = [{Text: "Education"}, {text: "Work Experience"}];
+
+export default function App() {
+  //return <ButtonBox buttons={ NAMES }/>;
+  return <AboutMe />;
+}
