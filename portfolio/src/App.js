c1c8cef8a6aae1b360d1aaaa0f6a02220627166e
TessaNyquist <tessanyquist@outlook.com>
2023-06-18 18:02:04 -0400

    Add contactMe outline

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -12,7 +12,7 @@ import Col from './react-bootstrap/Col';
 function AboutMe (){
   let bio = "A native Northern Virginian, I completed my B.A. in political science and B.S. in computer science from James Madison University (JMU) in 2022, and received the NSA Information Systems Security Professionals Certification.  While attending JMU, I assisted fellow students navigate the university’s disciplinary process while I served on the executive board of Student  Defenders. Currently, I am serving as a legal assistant at an employment law firm in Washington, D.C., where I assist prospective clients in navigating the firm’s intake process, while leveraging my background in computer science to automate processes. I am passionate about serving back in the community I grew up in and is interested in pursuing opportunities in the tech industry in the Washington, D.C. metro.";
   return (
-    <Container>
+    <Container fluid>
       <Row> About Me</Row>
       <Figure>
         <Figure.Image 
@@ -30,9 +30,23 @@ function AboutMe (){
 //const NAMES = [{Text: "Education"}, {text: "Work Experience"}];
 function Header() {
   return (  
-    <Container>
+    <Container fluid>
       <Row>
         <Col>Header </Col>
+      </Row>
+    </Container>);
+}
+function ContactMe() {
+  return (  
+    <Container fluid>
+      <Row> 
+        Email
+      </Row>
+      <Row>
+        Name
+      </Row>
+      <Row>
+        Message
       </Row>
     </Container>);
 }
@@ -41,9 +55,10 @@ function Header() {
 export default function App() {
   //return <ButtonBox buttons={ NAMES }/>;
   return (
-    <Container>
+    <Container fluid>
       <Header />
       <AboutMe />
+      <ContactMe />
     </Container>
     );
 }
