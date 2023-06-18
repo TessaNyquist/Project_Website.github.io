df102bf486a37c9cde735332fe82a0a3dc861453
TessaNyquist <tessanyquist@outlook.com>
2023-06-18 17:46:48 -0400

    Image now displaying

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -24,7 +24,6 @@ import './App.css';
 
 function AboutMe (){
   let bio = "A native Northern Virginian, I completed my B.A. in political science and B.S. in computer science from James Madison University (JMU) in 2022, and received the NSA Information Systems Security Professionals Certification.  While attending JMU, I assisted fellow students navigate the university’s disciplinary process while I served on the executive board of Student  Defenders. Currently, I am serving as a legal assistant at an employment law firm in Washington, D.C., where I assist prospective clients in navigating the firm’s intake process, while leveraging my background in computer science to automate processes. I am passionate about serving back in the community I grew up in and is interested in pursuing opportunities in the tech industry in the Washington, D.C. metro.";
-  //let imgsrc = "./Images/Tessa-Banner.png"
   return (
     <div>
       <h2> About Me</h2>
